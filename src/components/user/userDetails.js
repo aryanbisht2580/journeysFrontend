37792b3b585deff5e2997fedee95e2fd006d20ae
aryanbisht2580 <aryanbisht2580@gmail.com>
2024-06-20 22:18:32 +0530

    removing address issue

--- src/components/user/userDetails.js
+++ src/components/user/userDetails.js
@@ -9,7 +9,6 @@ const UserDetails = () => {
     <h1 className='heading text-center'>User Details</h1>
     <h3>Name: {data.user.name}</h3>
     <h3>Email: {data.user.email}</h3>
-    <h3>Address: {data.user.address}</h3>
     <h3>Phone: +91-{data.user.phone}</h3>
     </>
   )
